fdca830381b2776049324b1209920f48f6c58bc2
Armen Juhl <armenjuhl@gmail.com>
2019-12-03 13:49:45 -0800

    add gatsby demo project to personal github

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,17 @@
 import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
+import Layout from "../components/layout"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <Layout>
+    <Link to="/contact/">Contact</Link>
+    <Header headerText="Hello Gatsby!"/>
+    <p>What a galaxy.</p>
+    <img src="https://source.unsplash.com/random/400x200" alt=""/>
+    <br />
+    <form method="get" action="/about">
+      <button className="primaryButton pt10">Learn more</button>
+    </form>
+  </Layout>
+)
